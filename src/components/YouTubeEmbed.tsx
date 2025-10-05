64e15ae8fe3445af50566151afce1f3b0c7f9f70
Nonce Firewall <noncefirewall@gmail.com>
2025-10-05 01:23:24 +0100

    Updated index.css

--- src/components/YouTubeEmbed.tsx
+++ src/components/YouTubeEmbed.tsx
@@ -7,15 +7,17 @@ interface YouTubeEmbedProps {
 
 const YouTubeEmbed: React.FC<YouTubeEmbedProps> = ({ videoId, title = 'YouTube Video' }) => {
   return (
-    <div className="relative my-6 rounded-xl overflow-hidden shadow-lg">
-      <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
-        <iframe
-          src={`https://www.youtube.com/embed/${videoId}?modestbranding=1&rel=0&showinfo=0&controls=1&fs=1&cc_load_policy=0&iv_load_policy=3&autohide=1`}
-          title={title}
-          className="absolute top-0 left-0 w-full h-full rounded-xl border-0"
-          allowFullScreen
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        />
+    <div className="flex justify-center my-8 w-full">
+      <div className="relative w-full max-w-4xl rounded-xl overflow-hidden shadow-lg">
+        <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
+          <iframe
+            src={`https://www.youtube.com/embed/${videoId}?modestbranding=1&rel=0&showinfo=0&controls=1&fs=1&cc_load_policy=0&iv_load_policy=3&autohide=1`}
+            title={title}
+            className="absolute top-0 left-0 w-full h-full rounded-xl border-0"
+            allowFullScreen
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          />
+        </div>
       </div>
     </div>
   )
